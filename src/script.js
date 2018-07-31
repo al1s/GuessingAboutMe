69d9df8a68815b4f764636772587ec2410aabee5
alstof <alstof@gmail.com>
2018-07-31 14:01:15 -0700

    Add game flow description

--- src/script.js
+++ src/script.js
@@ -4,3 +4,15 @@
 /* eslint prefer-const: 0 */
 /* eslint arrow-parens: 0 */
 /* eslint no-restricted-syntax: 0 */
+
+// Game flow:
+// 1. Greet the user.
+// 2. Ask the user name.
+// 3. Ask whether he wants to play.
+// 4. Yes => Start loop with questions
+// 4.1. Do you believe my full name is Aleksandr Vladimirovich?
+// 4.2 I like motocycles. And all of those I had were - Harley-Davidson. Do you believe it?
+// 4.3 I'm from Russia. And in Russia it's forbidden to talk during the class hours. And those who violate this order are expelled from school after a second official warning?
+// 4.4 I went to Tibet. And climbed Everest - the North Face of it.
+// 4.5 I came to the USA in a middle of 2016. And worked in 10 different states, including Texas, Massachusetts, Colorado, California, New York, Nevada and North Carolina. Do you believe it?
+// 5. No => Sorry for inconvenience, the other parts of the page is under construction. So would you like to play?
