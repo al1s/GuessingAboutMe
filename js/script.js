bc806a09e9c9cd85f4971de852d1885842f109f3
alstof <alstof@gmail.com>
2018-08-01 15:01:31 -0700

    Add sixth question to UI

--- js/script.js
+++ js/script.js
@@ -24,97 +24,153 @@
 // (style) orginize fact concisely on the layout
 // (engine) add a question with controlled numeric input; four opps to answer; with tips whether the answer is too high or too low
 // (engine) add a question with multiple correct answers stored in array; six tries to get any correct answer; with a rigth answers displayed whether the user guessed right or attempts exhausted
+// (engine) add final message with total number of questions answered correctly
 
-() => {
-  // Greet and ask for the user name
-  var userName = prompt('My name is Alex. What is your name?');
-  alert('Hello ' + userName + '. I\'d like to offer you play a guessing game.');
-  console.log('The user entered: ' + userName);
+// Greet and ask for the user name
+var userName = prompt('My name is Alex. What is your name?');
+alert('Hello ' + userName + '. I\'d like to offer you play a guessing game.');
+console.log('The user entered: ' + userName);
 
-  // questions' pool
-  var questName = 'Do you believe my full name is Aleksandr Vladimirovich?';
-  var questMotoManufacturer =
-    'I like motocycles. And all of those I had ' +
-    'were - Harley-Davidson. Do you believe it?';
-  var questRussianSchools =
-    'I\'m from Russia. And in russian schools it\'s forbidden to talk during the class ' +
-    'hours. And those who violate this rule are expelled from school after a ' +
-    'second official warning?';
-  var questTibet =
-    'I went to Tibet. And climbed Everest - the North Face of it. Do you think it\'s true?';
-  var questStates =
-    'I came to the USA in a middle of 2016. Since than I\'ve worked in 10 ' +
-    'different states, including Texas, Massachusetts, Colorado, California, ' +
-    'New York, Nevada and North Carolina. Do you believe it?';
-  var questNumeric = 'What is the meaning of life if it filled with surprises?';
-  var questMultiCorrect = 'What country besides the USA and Russia I\'ve been?';
+// questions' pool
+var questName = 'Do you believe my full name is Aleksandr Vladimirovich?';
+var questMotoManufacturer =
+  'I like motocycles. And all of those I had ' +
+  'were - Harley-Davidson. Do you believe it?';
+var questRussianSchools =
+  'I\'m from Russia. And in russian schools it\'s forbidden to talk during the class ' +
+  'hours. And those who violate this rule are expelled from school after a ' +
+  'second official warning?';
+var questTibet =
+  'I went to Tibet. And climbed Everest - the North Face of it. Do you think it\'s true?';
+var questStates =
+  'I came to the USA in a middle of 2016. Since than I\'ve worked in 10 ' +
+  'different states, including Texas, Massachusetts, Colorado, California, ' +
+  'New York, Nevada and North Carolina. Do you believe it?';
+var questNumeric = 'What is the meaning of life if it\'s filled with surprises?';
+var questMultiCorrect = 'What country besides the USA and Russia I\'ve been?';
 
-  var answerTrue = 'You are correct!';
-  var answerFalse = 'It\'s not correct!';
+var answerTrue = 'You are correct!';
+var answerFalse = 'It\'s not correct!';
+var answerHigh = 'Too high!';
+var answerLow = 'Too low!';
+var attemptsExhausted = 'Exhausted all attempts!';
 
-  // answers' pool
-  var answerName =
-    'It\'s indeed long. I mean first + ' +
-    'middle together are \'Aleksandr Vladimirovich\'.';
-  var answerMotoManufacturer = 'They were BMW.';
-  var answerRussianSchools =
-    'Russian schools are stricter then schools in the USA. ' +
-    'But not such strict.';
-  var answerTibet =
-    'I was in Tibet and I was at the Everest\'s base camp, ' +
-    'but I\'m not an alpinist and never climbed big mountains.';
-  var answerStates = 'I\'ve worked only in Greater Seattle area.';
-  var answerMultiCorrect = [
-    'Mongolia',
-    'Spain',
-    'China',
-    'Italy',
-    'France',
-    'Switzerland',
-    'Netherlands'
-  ];
-  var meaningLife = 42;
-  var answerNumeric = meaningLife + Math.floor(Math.random() * meaningLife);
+// answers' pool
+var answerName =
+  'It\'s indeed long. I mean first + ' +
+  'middle together are \'Aleksandr Vladimirovich\'.';
+var answerMotoManufacturer = 'They were BMW.';
+var answerRussianSchools =
+  'Russian schools are stricter then schools in the USA. ' +
+  'But not such strict.';
+var answerTibet =
+  'I was in Tibet and I was at the Everest\'s base camp, ' +
+  'but I\'m not an alpinist and never climbed big mountains.';
+var answerStates = 'I\'ve worked only in Greater Seattle area.';
+var answerMultiCorrect = [
+  'Mongolia',
+  'Spain',
+  'China',
+  'Italy',
+  'France',
+  'Switzerland',
+  'Netherlands'
+];
+var meaningLife = 42;
+var answerNumeric = meaningLife; //+ Math.floor(Math.random() * meaningLife);
+var correctAnswersCounter = 0;
 
-  // UI part
-  var userInputName = prompt(questName);
-  console.log('The user guess for the 1st question: ' + userInputName);
-  if (gotYes(userInputName)) alert(answerTrue + ' ' + answerName);
-  else alert(answerFalse + ' ' + answerName);
+// UI part
+/*
+// block of yes/no questions
+var userInputName = prompt(questName);
+console.log('The user guess for the 1st question: ' + userInputName);
+if (gotYes(userInputName)) {
+  alert(answerTrue + ' ' + answerName);
+  correctAnswersCounter += 1;
+}
+else alert(answerFalse + ' ' + answerName);
 
-  var userInputMotoManufacturer = prompt(questMotoManufacturer);
+var userInputMotoManufacturer = prompt(questMotoManufacturer);
+console.log(
+  'The user guess for the 2nd question: ' + userInputMotoManufacturer
+);
+if (gotYes(userInputMotoManufacturer))
+  alert(answerFalse + ' ' + answerMotoManufacturer);
+else {
+  alert(answerTrue + ' ' + answerMotoManufacturer);
+  correctAnswersCounter += 1;
+}
+
+var userInputRussianSchools = prompt(questRussianSchools);
+console.log('The user guess for the 3rd question: ' + userInputRussianSchools);
+if (gotYes(userInputRussianSchools))
+  alert(answerFalse + ' ' + answerRussianSchools);
+else {
+  alert(answerTrue + ' ' + answerRussianSchools);
+  correctAnswersCounter += 1;
+}
+
+var userInputTibet = prompt(questTibet);
+console.log('The user guess for the 4th question: ' + userInputTibet);
+if (gotYes(userInputTibet)) alert(answerFalse + ' ' + answerTibet);
+else {
+  alert(answerTrue + ' ' + answerTibet);
+  correctAnswersCounter += 1;
+}
+
+var userInputStates = prompt(questStates);
+console.log('The user guess for the 5th question: ' + userInputStates);
+if (gotYes(userInputStates)) alert(answerFalse + ' ' + answerStates);
+else {
+  alert(answerTrue + ' ' + answerStates);
+  correctAnswersCounter += 1;
+}
+*/
+// numeric question
+var i = 4;
+console.log('The random answer: ' + answerNumeric);
+do {
+  // ask for the number and check input type
+  var userInputNumeric = prompt(questNumeric);
   console.log(
-    'The user guess for the 2nd question: ' + userInputMotoManufacturer
+    'Expected number, got ' +
+      userInputNumeric +
+      '. Is it a number: ' +
+      !isNaN(Number(userInputNumeric))
   );
-  if (gotYes(userInputMotoManufacturer))
-    alert(answerFalse + ' ' + answerMotoManufacturer);
-  else alert(answerTrue + ' ' + answerMotoManufacturer);
-
-  var userInputRussianSchools = prompt(questRussianSchools);
-  console.log(
-    'The user guess for the 3rd question: ' + userInputRussianSchools
-  );
-  if (gotYes(userInputRussianSchools))
-    alert(answerFalse + ' ' + answerRussianSchools);
-  else alert(answerTrue + ' ' + answerRussianSchools);
-
-  var userInputTibet = prompt(questTibet);
-  console.log('The user guess for the 4th question: ' + userInputTibet);
-  if (gotYes(userInputTibet)) alert(answerFalse + ' ' + answerTibet);
-  else alert(answerTrue + ' ' + answerTibet);
-
-  var userInputStates = prompt(questStates);
-  console.log('The user guess for the 5th question: ' + userInputStates);
-  if (gotYes(userInputStates)) alert(answerFalse + ' ' + answerStates);
-  else alert(answerTrue + ' ' + answerStates);
-
-  // little helper function
-  function gotYes(input) {
-    var inputLowerCase = input.toString().toLowerCase();
-    return (
-      inputLowerCase === 'yes' ||
-      inputLowerCase === 'yep' ||
-      inputLowerCase === 'y'
+  while (isNaN(Number(userInputNumeric))) {
+    userInputNumeric = prompt(questNumeric);
+    console.log(
+      'Expected number, got ' +
+        userInputNumeric +
+        '. Is it a number: ' +
+        !isNaN(Number(userInputNumeric))
     );
   }
-};
+
+  // check whether the user answer is correct
+  userInputNumeric = Number(userInputNumeric);
+  i -= 1;
+  console.log(i + ' steps left');
+  if (userInputNumeric > answerNumeric)
+    alert(answerHigh + ' Attempts left: ' + i);
+  else if (userInputNumeric < answerNumeric)
+    alert(answerLow + ' Attempts left: ' + i);
+  else if (userInputNumeric === answerNumeric) {
+    alert(answerTrue);
+    correctAnswersCounter += 1;
+    console.log('Answer is correct');
+  }
+  if (i === 0 && userInputNumeric !== answerNumeric) alert(attemptsExhausted);
+} while (i > 0 && userInputNumeric !== answerNumeric);
+
+// little helper function
+function gotYes(input) {
+  var inputLowerCase = input.toString().toLowerCase();
+  return (
+    inputLowerCase === 'yes' ||
+    inputLowerCase === 'yep' ||
+    inputLowerCase === 'y'
+  );
+}
